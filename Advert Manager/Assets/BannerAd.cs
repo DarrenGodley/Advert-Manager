7ca7bfa83ba247c972327e4024ac2ab19d88189a
DarrenGodley <32136209+DarrenGodley@users.noreply.github.com>
2020-02-25 10:50:06 +0000

    RewardedVideo

---
+++ Advert Manager/Assets/BannerAd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAd : MonoBehaviour
+{
+
+    public string gameId = "3483556";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+    }
+        IEnumerator ShowBannerWhenReady()
+        {
+            while (!Advertisement.IsReady(placementId))
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+            Advertisement.Banner.Show(placementId);
+        }    
+}

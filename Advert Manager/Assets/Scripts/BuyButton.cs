4dac537e8b30b0cf4dacb87981971d92cb03513f
DarrenGodley <32136209+DarrenGodley@users.noreply.github.com>
2020-04-20 21:32:13 +0100

    IAP
    20/04/2020

---
+++ Advert Manager/Assets/Scripts/BuyButton.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyButton : MonoBehaviour
+{
+
+    public enum ItemType
+    {
+        Gold50,
+        Gold100,
+        NoAds
+    }
+
+    public ItemType itemType;
+
+    public Text priceText;
+
+    private string defaultText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultText = priceText.text;
+        StartCoroutine(LoadPriceRoutine());
+    }
+
+    public void clickBuy()
+    {
+        switch (itemType)
+        {
+            case ItemType.Gold50:
+                IAPmanager.Instance.Buy50Gold();
+                break;
+
+            case ItemType.Gold100:
+                IAPmanager.Instance.Buy100Gold();
+                break;
+
+            case ItemType.NoAds:
+                IAPmanager.Instance.BuyNoAds();
+                break;
+        }
+    }
+
+    private IEnumerator LoadPriceRoutine()
+    {
+        while (!IAPmanager.Instance.IsInitialized())
+
+            yield return null;
+
+            string loadedPrice = "";
+
+            switch (itemType)
+            {
+                case ItemType.Gold50:
+                loadedPrice = IAPmanager.Instance.getProductPriceFromStore(IAPmanager.Instance.GOLD_50);
+                    break;
+
+                case ItemType.Gold100:
+                loadedPrice = IAPmanager.Instance.getProductPriceFromStore(IAPmanager.Instance.GOLD_100);
+                    break;
+
+                case ItemType.NoAds:
+                loadedPrice = IAPmanager.Instance.getProductPriceFromStore(IAPmanager.Instance.NO_ADS);
+                    break;
+            }
+        priceText.text = defaultText + " " + loadedPrice;
+    }
+}
---
+++ Advert Manager/Assets/Scripts/DataManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataManager : Singleton<DataManager>
+{
+    public Text goldAmountText;
+
+    public GameObject noAdsButton;
+
+    private int goldAmount = 0;
+
+    private bool noAds = false;
+
+    public void addGold(int amount)
+    {
+        goldAmount += amount;
+        goldAmountText.text = goldAmount.ToString();
+    }
+
+    public void removeAds()
+    {
+        noAds = true;
+        noAdsButton.SetActive(false);
+    }
+}
